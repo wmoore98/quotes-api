c1c7e6b94fc0c8e30badf59252d277582062d081
Bill <wmoore98@sbcglobal.net>
2019-09-19 14:04:31 -0400

    Update CORS to github

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cors);
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://wmoore98.github.io'); // update to match the domain you will make the request from
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
